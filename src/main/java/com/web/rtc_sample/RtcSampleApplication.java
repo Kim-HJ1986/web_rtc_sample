90369c6837bc9eec4ab95540a27b05eacf05e47a
Jun Kim <rlafbf222@naver.com>
2022-06-24 21:00:35 +0900

    RTC 샘플

---
+++ src/main/java/com/web/rtc_sample/RtcSampleApplication.java
@@ -0,0 +1,13 @@
+package com.web.rtc_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtcSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/rtc_sample/RtcSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.rtc_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
